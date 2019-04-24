67b96812bb07052c9bf864dc15b261c547b95c8d
Daniel Janus <dj@danieljanus.pl>
2019-04-24 16:52:53 +0200

    Add code to fix all bugs.

---
+++ client/guess.c
@@ -0,0 +1,123 @@
+#define __ __LINE__
+#define ha char
+#define iii ii(
+#define ii(x) ha x = __;
+#define i ,
+#define koniec );}
+#define liczby main(){printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
+#define juz 0
+
+iii raz)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+iii dwa)
+iii trzy)
+
+
+
+
+
+
+
+
+
+
+iii cztery)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+iii piec)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+iii szesc)
+iii siedem)
+
+
+
+
+
+
+iii osiem)
+
+
+iii dziewiec)
+
+
+iii dziesiec)
+
+
+
+
+iii jedenascie)
+
+liczby piec i siedem i osiem i osiem i dziewiec i cztery i dwa 
+i jedenascie i dziewiec i dziesiec i osiem i szesc i trzy i raz
+i juz koniec
